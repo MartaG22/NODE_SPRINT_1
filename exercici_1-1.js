74f4ffd24c9a6fab2f73c7fd862703be414303b5
Marta <martaxgarijo@gmail.com>
2022-05-15 23:56:08 +0200

    Afegeixo exercicis de l'Entrega1

---
+++ exercici_1-1.js
@@ -0,0 +1,47 @@
+// Nivell 1 - Exercici 1
+function exercici1_1(nom, cognom) {
+    console.log(nom, cognom);
+}
+exercici1_1("Perico", "Palotes");
+
+// Nivell 2 - Exercici 1
+let nom1 = "Manolito";
+let cognom1 = "García";
+console.log(`${nom1} ${cognom1}`);
+
+// Nivell 2 - Exercici 2
+function exercici2_2(valor) {
+    console.log(valor);
+}
+let valor = "He aconseguit fer l'exercici!"
+exercici2_2(`${valor}`);
+
+
+// Nivell 3 - Exercici 1
+
+let miArray = [];
+let contador = "";
+let nuevoNumero;
+function model() {
+    let contador = "";
+    for (let i = 0; i <= 9; i++) {
+        nuevoNumero = i.toString();
+        if (i < 9) {
+            contador += `${nuevoNumero}, `;
+        } else {
+            contador += nuevoNumero;
+        }
+    }
+    console.log(contador)
+    return contador;
+}
+for (let j = 0; j < 10; j++) {
+    //let nombreFuncion = f + j;
+    nuevoContador = model();
+    miArray.push(nuevoContador);
+}
+
+
+// Nivell 3 - Exercici 2
+
+let variable = (function (nom) { console.log(nom) })("Marta");
