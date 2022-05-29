20b81cd17654e2f292754c00decbbc03880817e5
Marta <martaxgarijo@gmail.com>
2022-05-30 00:10:36 +0200

    Afegeixo les correccions del exercicis de l'entrega 4, de l'sprint 1

--- Entrega_4.js
+++ Entrega_4.js
@@ -1,4 +1,6 @@
-// Nivell 1 - Exercici 1
+//! Entrega 1.4: Async / Await
+
+// Nivell 1 - Exercici 1   //* --- CORREGIT
 
 /* Crea una funció asíncrona que rebi un id d'empleat i imprimeixi per pantalla el nom de l'empleat i el seu salari, 
  usant les funcions getEmployee() i getSalary() que has definit a la tasca anterior.*/
@@ -26,8 +28,8 @@ let salaries = [{
     salary: 2000
 }];
 
-
-function getEmployee(position) {
+//* Exercici antic:
+/*function getEmployee(position) {
     return new Promise(function (resolve, reject) {
         if (foundEmployee != -1) {
             const currentEmployee = employees[position].name;
@@ -65,78 +67,155 @@ function foundEmployee(worker) {
     return worker.id === getId;
 }
 let positionEmployee = (employees.findIndex(foundEmployee));
-asyncFunction(positionEmployee);
+asyncFunction(positionEmployee);   */
+
+//*Exercici  Corregit  ------------------     (29-5-22)     --------------------
+
+const getEmployee = (id) => {
+    return new Promise((resolve, reject) => {
+
+        let positionEmployee = (employees.findIndex(found => found.id === id));
+        console.log("posicion:", positionEmployee)
+        if (positionEmployee != -1) {
+            resolve(`L'empleat amb id ${id} s'ha trobat, ha estat ${employees[positionEmployee].name}!`);
+        } else {
+            reject(`No s'ha trobat el treballador amb l'ID ${id}`);
+        }
+    });
+}
+
+getEmployee(3)
+    .then(resolve => console.log(resolve))
+    .catch(error => console.log(error))
+
+const getSalary = (id) => {
+    return new Promise((resolve, reject) => {
+
+        let positionEmployee = (salaries.findIndex(found => found.id === id));
+        console.log("posicion:", positionEmployee)
+        if (positionEmployee != -1) {
+            resolve(`L'empleat amb id ${id} té  ${salaries[positionEmployee].salary} € de salari!`);
+        } else {
+            reject(`No s'ha trobat el treballador amb l'ID ${id}`);
+        }
+    });
+}
+
+getSalary(3)
+    .then(resolve => console.log(resolve))
+    .catch(error => console.log(error))
 
 
+async function asyncFunction(id) {
+    try {
+        const currentEmployee = await getEmployee(id);
+        const currentSalary = await getSalary(id);
+        console.log(`L'empleat amb l'ID ${position} és: ${currentEmployee}, i té ${currentSalary} € de salari`);
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 
-// Nivell 1 - Exercici 2
+// Nivell 1 - Exercici 2   //* --- CORREGIT
 
 /* Crea una nova funció asíncrona que cridi a una altra que retorni una Promise
  que efectuï la seva funció resolve() després de 2 segons de la seva invocació.*/
 
-const sumAfterTwoSeconds = (a, b) => {
+//*Exercici antic:
+/* const sumAfterTwoSeconds = (a, b) => {
     return new Promise(resolve => setTimeout(() => resolve(a + b), 2000))
 }
-
 async function asyncOperation() {
     const result = sumAfterTwoSeconds(4, 5);
     return (result);
 }
-
-asyncOperation().then(total => console.log('Promise:', total));
-
+asyncOperation().then(total => console.log('Promise:', total));  */
 
 
-// Nivell 2 - Exercici 1
+//*Exercici  Corregit  ------------------     (29-5-22)     --------------------
+
+const sumAfterTwoSeconds = (a, b) => {
+    return new Promise(resolve => setTimeout(() => resolve(a + b), 2000));
+}
+
+async function asyncOperation(a, b) {
+    try {
+        const result = await sumAfterTwoSeconds(a, b);
+        return console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+asyncOperation(4, 5);
+
+
+
+// Nivell 2 - Exercici 1   //* --- CORREGIT
 
 /* Crea una funció que retorni el doble del número que se li passa com a paràmetre després de 2 segons.
  Crea una altra funció que rebi tres números i calculi la suma dels seus dobles usant la funció anterior.*/
 
-/* Aquest exercici em funciona si comento la resta d'exercicis, però si estan tots sense comentar no em funciona.*/
+
+//*Exercici  Corregit  ------------------     (29-5-22)     --------------------
 
 const doubleAfterTwoSeconds = (a) => {
-    return new Promise(resolve => setTimeout(() => resolve(a * 2), 2000))
+    let result = a * 2;
+    let message = `${a} * 2 = ${result}`
+    return new Promise(resolve => setTimeout(() => resolve(message), 2000))
 }
 
-async function asyncOperation() {
-    let number1 = parseInt(Math.random() * 100);
-    console.log("number1 =", number1);
-    const result = doubleAfterTwoSeconds(number1);
-    return (result);
+async function asyncDoubleNumber(number1) {
+    try {
+        const result = await doubleAfterTwoSeconds(number1);
+        console.log(result)
+        // console.log((`${number1} * 2 = ${result}`));
+    } catch (error) {
+        console.log("Nivell 2 - Exercici 1A:  No s'ha pogut realitzar l'operació");
+    }
 }
-
-asyncOperation().then(total => console.log('El doble és', total));
-
+asyncDoubleNumber(parseInt(Math.random() * 100))
 
 
-/* Aquest exercici em funciona si comento la resta d'exercicis, però si estan tots sense comentar no em funciona.*/
+
+
+// Aquest és l'exercici que no em funcionava bé.
+// Era perquè el nom de la funció asíncrona era el mateix que la funció de l'exercici anterior.
+
+//*Exercici  Corregit  ------------------     (29-5-22)     --------------------
 
 const sumThreeNumbers = (b) => {
     let doubleNumber;
     let sumOfNumbers = 0;
+    let message = "La suma del doble dels nombres ";
     for (let j = 0; j < 3; j++) {
-        console.log(j, b[j]);
         doubleNumber = b[j] * 2;
         sumOfNumbers += doubleNumber;
+        message += `${b[j]} `;
     }
-    return new Promise(resolve => setTimeout(() => resolve(sumOfNumbers), 2000))
+    let result = `${message} és: ${sumOfNumbers}`
+    return new Promise(resolve => setTimeout(() => resolve(result), 2000));
 }
 
-async function asyncOperation() {
+async function asyncSumNumbers(b) {
+    try {
+        const result = await sumThreeNumbers(b);
+        console.log(result);
+    } catch (error) {
+        console.log("Nivell 2 - Exercici 1B: No s'ha pogut realitzar l'operació");
+    }
+}
+
+(function threeNumbers() {
     const b = [];
     for (let i = 0; i < 3; i++) {
         let number1 = parseInt(Math.random() * 20);
         b.push(number1);
     }
     console.log("arrayNumbers =", b);
-    const result = sumThreeNumbers(b);
-    return (result);
-}
-
-asyncOperation().then(total => console.log("La suma del doble d'aquests nombres és:", total));
-
-
+    asyncSumNumbers(b)
+})()
 
 
 // Nivell 3 - Exercici 1
