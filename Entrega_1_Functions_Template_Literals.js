97911eb93c3fbbd2e5b6543b6cbc4b9deaeb0c94
Marta <martaxgarijo@gmail.com>
2022-07-26 21:44:26 +0200

    Exercicis Corregits

--- Entrega_1_Functions_Template_Literals.js
+++ Entrega_1_Functions_Template_Literals.js
@@ -1,27 +1,71 @@
 //! Functions & Template Literals
 
-// Nivell 1 - Exercici 1 //!  -- OK!!!
+// Nivell 1 - Exercici 1 //?  -- OK!!!
 function exercici1_1(nom, cognom) {
     console.log(nom, cognom);
 }
 exercici1_1("Perico", "Palotes");
 
-// Nivell 2 - Exercici 1 //!  -- OK!!!
+
+
+// Nivell 2 - Exercici 1 //?  -- OK!!!
 let nom1 = "Manolito";
 let cognom1 = "García";
 console.log(`${nom1} ${cognom1}`);
 
-// Nivell 2 - Exercici 2  //! REPETIR -- 0 PUNTOS
-function exercici2_2(valor) {
-    console.log(valor);
+
+
+// Nivell 2 - Exercici 2  //!  CORREGIT  26-7-22
+// Invoca una funció que retorni un valor des de dins d'una template literal.
+
+function frase(exercici) {
+    return `He aconseguit fer l'${exercici}`;
 }
-let valor = "He aconseguit fer l'exercici!"
-exercici2_2(`${valor}`);
+
+let valor = "Exercici 2 del Nivell 2";
+console.log(`Resultat: ${frase(valor)}`);
 
 
-// Nivell 3 - Exercici 1  //! REPETIR -- 0 PUNTOS
+
+
+// Nivell 3 - Exercici 1  //!  CORREGIT  26-7-22
+// Crea una matriu de deu funcions i emplena-la mitjançant un bucle de manera que cada funció compti del 0 al 9 per la consola.
+// Invoca cada funció de l'array iterativament. Haurà de mostrar-se per consola el compte del 0 al 9 deu vegades.
+//? - Al N3 E1 no estàs guardant còpies de la funció i excecutant-les després en bucle, estàs fent-ho tot alhora (i lo segon abans que lo primer).
+//?També és molt confos que una funció faci alhora return i console.log (només hauria de fer el segon)
 
 let miArray = [];
+function comptar() {
+    let nouNumero;
+    let comptador = "";
+    for (let i = 0; i <= 9; i++) {
+        nouNumero = i.toString();
+        if (i < 9) {
+            comptador += `${nouNumero}, `;
+        } else {
+            comptador += nouNumero;
+        }
+    }
+    console.log(comptador);
+}
+
+function model() {
+    for (let j = 0; j < 10; j++) {
+        miArray.push(comptar);
+    }
+}
+
+function mostrarComptes() {
+    miArray.forEach(funcio => funcio())
+}
+
+model();
+mostrarComptes();
+
+
+
+// (*(*(Exercici anterior)*)*)
+/*let miArray = [];
 let contador = "";
 let nuevoNumero;
 function model() {
@@ -41,9 +85,11 @@ for (let j = 0; j < 10; j++) {
     //let nombreFuncion = f + j;
     nuevoContador = model();
     miArray.push(nuevoContador);
-}
+}*/
 
 
-// Nivell 3 - Exercici 2   //!  -- OK!!!
 
+
+// Nivell 3 - Exercici 2   //?  -- OK!!!
+// Crea una funció anònima autoinvocable igualada a una variable que mostri per consola el nom de l'usuari/ària a rebut com a paràmetre.
 let variable = (function (nom) { console.log(nom) })("Marta");
